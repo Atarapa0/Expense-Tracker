cabdb2d2e5811e6ac98bb70aa1769528949b932e
Furkan Erdoğan <furkaan.er@outlook.com>
2025-11-22 16:00:52 +0300

    feat(transactions): Enhance transaction management with add and load functionality, update dependencies

--- lib/providers/transaction_provider.dart
+++ lib/providers/transaction_provider.dart
@@ -1,12 +1,76 @@
-import 'dart:convert';
 import 'package:expense_tracker/models/transaction_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
 
+class TransactionProvider with ChangeNotifier {
+ TransactionProvider(this.box);
+
+  final Box<TransactionModel> box;
+  int _idCounter = 1;
+  final List<TransactionModel> _items = [];
+  List<TransactionModel> get items => List.unmodifiable(_items);
+
+
+  Future<void> addTransaction(
+  String title,
+  double amount,
+  DateTime dateTime,
+  TransactionType type,
+  Category category,
+) async {
+  if (amount <= 0) return;
+
+  final box = Hive.box<TransactionModel>('transactions');
+
+  final tx = TransactionModel(
+    id: _idCounter++,
+    title: title,
+    amount: amount,
+    dateTime: dateTime,
+    type: type,
+    category: category,
+  );
+
+  // 1) Listeye ekle
+  _items.add(tx);
+
+  // 2) Hive'e kaydet
+  await box.put(tx.id, tx);
+
+  // 3) Listeyi tarihe göre sırala
+  _items.sort((a, b) => b.dateTime.compareTo(a.dateTime));
+
+  notifyListeners();
+}
+  double get income {
+    return _items
+        .where((tx) => tx.type == TransactionType.income)
+        .fold(0, (sum, tx) => sum + tx.amount);
+  }
+  double get expense {
+    return _items
+        .where((tx) => tx.type == TransactionType.expense)
+        .fold(0, (sum, tx) => sum + tx.amount);
+  }
+  double get balance => income - expense;
+
+Future<void> loadTransactions() async {
+  final box = Hive.box<TransactionModel>('transactions');
+  _items.clear();
+  _items.addAll(box.values);
+  _items.sort((a, b) => b.dateTime.compareTo(a.dateTime));
+  notifyListeners();
+}
+}
+
+
+/*
 class TransactionProvider with ChangeNotifier {
   final List<TransactionModel> _items = [];
   List<TransactionModel> get items => List.unmodifiable(_items);
   int _idCounter = 1;
+
+  TransactionProvider(Box<TransactionModel> box);
   void addTransaction(
     String title,
     double amount,
@@ -58,3 +122,4 @@ class TransactionProvider with ChangeNotifier {
 
 }
 
+*/
